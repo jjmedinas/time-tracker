790701d03efe97976040cdb65e7d2c75d8714c9c
jjmedinas <juan.ms@outlook.com>
2019-12-16 22:34:52 -0500

    Show login error in a modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import '../stylesheets/application'
 import '../tracker/bootstrap_custom'
 import '../tracker/material_dashboard'
 import '../tracker/dashboard'
+import '../tracker/bootstrap_notify'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/tracker/dashboard.js
+++ app/javascript/tracker/dashboard.js
@@ -17,16 +17,12 @@ $(document).ready(function() {
       success: function() {
         window.location = '/dashboard'
       },
-      error: function() {
-        alert('User and password dont match.');
-      },
+      error: md.showLoginErrorNotification('top','center'),
       dataType: "json"
     });
 
     e.preventDefault();
   }
-
-  $("#session-login").click( submitForm );
 
   function logout( e ) {
     $.ajax({
@@ -45,5 +41,70 @@ $(document).ready(function() {
     e.preventDefault();
   }
 
+  function showUserProfile( e ){
+    e.preventDefault();
+    $('#user-profile-content').removeClass("hidden");
+    $('#list-users-content').addClass("hidden");
+    $('#all-users-report-content').addClass("hidden");
+
+    $("#user-profile-nav").addClass("active")
+    $("#list-users-nav").removeClass("active")
+    $("#all-users-report-nav").removeClass("active")
+    $('#history-nav').removeClass("active")
+    $('#track-time-nav').removeClass("active")
+  }
+
+  function showUsersList( e ){
+    e.preventDefault();
+    $('#list-users-content').removeClass("hidden");
+    $('#user-profile-content').addClass("hidden");
+    $('#all-users-report-content').addClass("hidden");
+
+    $("#list-users-nav").addClass("active")
+    $("#user-profile-nav").removeClass("active")
+    $("#all-users-report-nav").removeClass("active")
+  }
+
+  function showAllUsersReport( e ){
+    e.preventDefault();
+    $('#all-users-report-content').removeClass("hidden");
+    $('#user-profile-content').addClass("hidden");
+    $('#list-users-content').addClass("hidden");
+
+    $("#all-users-report-nav").addClass("active")
+    $("#user-profile-nav").removeClass("active")
+    $("#list-users-nav").removeClass("active")
+  }
+
+  function showTrackTime( e ){
+    e.preventDefault();
+    $('#track-time-content').removeClass("hidden");
+    $('#user-profile-content').addClass("hidden");
+    $('#history-content').addClass("hidden");
+
+    $('#track-time-nav').addClass("active")
+    $('#user-profile-nav').removeClass("active")
+    $('#history-nav').removeClass("active")
+  }
+
+  function showHistory( e ){
+    e.preventDefault();
+    $('#history-content').removeClass("hidden");
+    $('#user-profile-content').addClass("hidden");
+    $('#track-time-content').addClass("hidden");
+
+    $('#history-nav').addClass("active")
+    $('#user-profile-nav').removeClass("active")
+    $('#track-time-nav').removeClass("active")
+  }
+
+
+  $("#session-login").click( submitForm );
   $("#session-logout").click( logout );
+
+  $("#user-profile-link").click( showUserProfile );
+  $("#list-users-link").click( showUsersList );
+  $("#all-users-report-link").click( showAllUsersReport );
+  $("#track-time-link").click( showTrackTime );
+  $("#history-link").click( showHistory );
 });
